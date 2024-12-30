84b3a72b6d512c3735a263435145e1da2e24a424
One290 <lenya.dorovskoy@mail.ru>
2024-12-30 19:07:41 +0800

    gf79uy

--- infrastructure/persistence/sql/query.sql
+++ infrastructure/persistence/sql/query.sql
@@ -1,19 +1,24 @@
+-- CreateAdmin :exec
 INSERT INTO admins VALUES (
     $1, $2, $3, $4, $5
 );
 
+-- CreateUser :exec
 INSERT INTO users VALUES (
     $1, $2, $3, $4, $5
 );
 
+-- CreateGeneralExercise :exec
 INSERT INTO generalExercises VALUES (
     $1, $2, $3, $4
 );
 
+-- CreateUserExercise :exec
 INSERT INTO userExercises VALUES (
     $1, $2, $3, $4
 );
 
+-- CreateWorkout :exec
 INSERT INTO workouts VALUES (
     $1, $2, $3, $4, $5
 );
