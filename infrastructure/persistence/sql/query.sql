619876e367c864663672e48267c86a89be818886
One290 <lenya.dorovskoy@mail.ru>
2024-12-30 19:16:25 +0800

    sghfy

--- infrastructure/persistence/sql/query.sql
+++ infrastructure/persistence/sql/query.sql
@@ -1,24 +1,24 @@
 -- name: CreateAdmin :exec
-INSERT INTO admins VALUES (
+INSERT INTO admins (guid, createdAt, name, login, password) VALUES (
     $1, $2, $3, $4, $5
 );
 
 -- name: CreateUser :exec
-INSERT INTO users VALUES (
+INSERT INTO users (guid, createdAt, name, login, password) VALUES (
     $1, $2, $3, $4, $5
 );
 
 -- name: CreateGeneralExercise :exec
-INSERT INTO generalExercises VALUES (
+INSERT INTO generalExercises (guid, adminGuid, name, description) VALUES (
     $1, $2, $3, $4
 );
 
 -- name: CreateUserExercise :exec
-INSERT INTO userExercises VALUES (
+INSERT INTO userExercises (guid, userGuid, name, description) VALUES (
     $1, $2, $3, $4
 );
 
 -- name: CreateWorkout :exec
-INSERT INTO workouts VALUES (
+INSERT INTO workouts (guid, userGuid, scheduledFor, durationInMinutes, setsAndBreaks) VALUES (
     $1, $2, $3, $4, $5
 );
